9497fc7e1372ce19e86f41e8faf0f91446a876de
Yuri Smychkovich <yuri.smychkovich@sophos.com>
2025-04-25 13:46:22 -0400

    AWS version

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-mvn -T 2C clean install -Prelease -DskipTests -Dmaven.javadoc.skip=true -Dfindbugs.skip=true -Dcheckstyle.skip=true -Dlicense.skip=true -pl '!webui'
+mvn -T 4C clean install -Prelease -Dawssdk.version=2.15.79 -DskipTests -Dmaven.javadoc.skip=true -Dfindbugs.skip=true -Dcheckstyle.skip=true -Dlicense.skip=true -pl '!webui'
 
 mkdir -p integration/docker/build
 cp assembly/client/target/alluxio-assembly-client-2.9.5-jar-with-dependencies.jar integration/docker/build/alluxio-client-2.9.5.jar
